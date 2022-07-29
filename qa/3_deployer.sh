32e457aa6af96eafae76f129eed44fce10a3f97c
wenbin <wenbin.shi@gmail.com>
2022-07-29 10:38:52 +0800

    add Dockerfile

--- qa/3_deployer.sh
+++ qa/3_deployer.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 function buildDeployer(){
+  cp -r docker/deployer/Dockerfile optimism/Dockerfile
   cd optimism
-  docker build -f ops/docker/Dockerfile.packages -t davionlabs/test .
+  docker build -t davionlabs/deployer .
+
+  #clean the Dockerfile
+  rm -rf Dockerfile
+}
+
+
+function startDeployer(){
+  docker run --net bridge -itd   -p 8080:8081 -e "CONTRACTS_RPC_URL=http://172.17.0.1:9545"  -e "CONTRACTS_DEPLOYER_KEY=ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" -e "CONTRACTS_TARGET_NETWORK=local" --entrypoint "/opt/optimism/packages/contracts/deployer.sh"  --name=deployer ethereumoptimism/deployer
 }
 
 
 buildDeployer
+
+startDeployer
