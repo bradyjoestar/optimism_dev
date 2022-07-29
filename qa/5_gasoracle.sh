b8a4102ac0dce03f6a2e031e7e9b6c7509c7e652
wenbin <wenbin.shi@gmail.com>
2022-07-29 12:03:52 +0800

    update gas-oracle

--- qa/5_gasoracle.sh
+++ qa/5_gasoracle.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-
-function  buildGasOracle() {
-  # build l2-geth
-  cd optimism/
+function buildGasOracle(){
+  cd optimism
   docker build -f gas-oracle/Dockerfile -t davionlabs/gas-oracle .
-  cd ../
+
+  cd ..
 }
+
+
+function startGasOracle(){
+  docker run --net bridge -itd --restart unless-stopped -e "GAS_PRICE_ORACLE_ETHEREUM_HTTP_URL=http://172.17.0.1:9545" -e "GAS_PRICE_ORACLE_LAYER_TWO_HTTP_URL=http://172.17.0.1:8545" -e "GAS_PRICE_ORACLE_PRIVATE_KEY=0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba" --name=gas-oracle davionlabs/gas-oracle
+}
+
+
+buildGasOracle
+startGasOracle
